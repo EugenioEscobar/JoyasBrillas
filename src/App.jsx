096d8aa348bc7bead277ef33e0c3c72b4b0c789b
EugenioEscobar <eugenio.escobar18@gmail.com>
2023-12-01 15:04:20 -0300

    nav superior

--- src/App.jsx
+++ src/App.jsx
@@ -9,47 +9,51 @@ import { logotipo, BannerPrincipal } from "./assets";
 function App() {
   return (
     <div className="overflow-hidden ">
-      <nav className="font-josefin mt-4 flex items-center  justify-between px-20">
+      <div className="bg-yellow-900">
+        <p className="p-2">asd</p>
+      </div>
+      <nav className="mt-4 flex items-center justify-between  px-20 font-josefin">
         <img src={logotipo} alt="Joyas Brillas" className="w-[180px]" />
         <div className="ms-20 flex flex-grow md:flex-row">
           <a
             href="#"
-            className="text-bold px-5 text-black hover:text-secondary hover:underline hover:underline-offset-8 "
+            className="underline-hover duration-400 relative inline px-5 text-sm font-normal uppercase text-black transition-all ease-in-out hover:text-secondary"
           >
             Inicio
           </a>
           <a
             href="#"
-            className="text-bold px-5 text-black hover:text-secondary hover:underline hover:underline-offset-8 "
+            className="underline-hover duration-400 relative inline px-5 text-sm font-normal uppercase text-black transition-all ease-in-out hover:text-secondary"
           >
             Tienda
           </a>
           <a
             href="#"
-            className="text-bold px-5 text-black hover:text-secondary hover:underline hover:underline-offset-8 "
+            className="underline-hover duration-400 relative inline px-5 text-sm font-normal uppercase text-black transition-all ease-in-out hover:text-secondary"
           >
             Nosotros
           </a>
           <a
             href="#"
-            className="text-bold px-5 text-black hover:text-secondary hover:underline hover:underline-offset-8 "
+            className="underline-hover duration-400 relative inline px-5 text-sm font-normal uppercase text-black transition-all ease-in-out hover:text-secondary"
           >
             Contáctanos
           </a>
         </div>
         <div className="flex">
           <a href="#" className="p-4">
-            <FaHeart className="text-black hover:text-secondary" />
+            <FaHeart className="duration-400 text-black transition-all ease-in-out hover:text-secondary" />
           </a>
           <a href="#" className="p-4">
-            <FaCartPlus className="text-black hover:text-secondary"></FaCartPlus>
+            <FaCartPlus className="duration-400 text-black transition-all ease-in-out hover:text-secondary"></FaCartPlus>
           </a>
           <a href="#" className="p-4">
-            <CiSearch className="text-black hover:text-secondary" />
+            <CiSearch className="duration-400 text-black transition-all ease-in-out hover:text-secondary" />
           </a>
         </div>
       </nav>
       <div className="">
+        <img src={BannerPrincipal} alt="Tus joyas" />
         <img src={BannerPrincipal} alt="Tus joyas" />
       </div>
     </div>
