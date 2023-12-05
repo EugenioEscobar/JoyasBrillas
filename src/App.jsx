5592b6bf4005c9688eb555ab782dd862222eb09d
EugenioEscobar <eugenio.escobar18@gmail.com>
2023-12-05 17:01:04 -0300

    add filter categories

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,21 @@ import { HiPhone, HiMail } from "react-icons/hi";
 
 import styles from "./style";
 
-import { logotipo, BannerPrincipal } from "./assets";
+import {
+  logotipo,
+  BannerPrincipal,
+  neckless,
+  anklet,
+  bracelet,
+  earrings,
+  ring,
+} from "./assets";
 
 function App() {
   return (
     <div className="overflow-hidden font-josefin ">
       {/* Barra de anuncios Superior */}
-      <div className="bg-backDark flex justify-between px-24 py-2 text-white">
+      <div className="flex justify-between bg-backDark px-24 py-2 text-white">
         <ul className="ul hover:[&>li]:duration-400 flex flex-1 flex-row gap-3 text-sm [&>li>a]:font-normal [&>li]:transition-colors [&>li]:duration-700 hover:[&>li]:text-black hover:[&>li]:transition-colors">
           <li>
             <a href="#">Tw</a>
@@ -97,30 +105,72 @@ function App() {
         className="flex h-[65vh] w-full bg-cover bg-center "
         style={{ backgroundImage: `url(${BannerPrincipal})` }}
       >
-        <div className="ml-32 flex max-w-[700px] flex-col pt-44">
+        <div className="ml-[6rem] flex max-w-[700px] flex-col gap-y-4 pt-44">
           <div className="inline-block">
-            <span className="font-ebGaramond inline-block bg-gradient-to-r from-amber-600 via-primary to-primary bg-clip-text  text-2xl font-bold capitalize text-transparent">
-              Diseños elegantes
+            <span className="inline-block bg-gradient-to-r from-yellow-200 to-primary bg-clip-text font-ebGaramond text-xl font-bold capitalize italic text-transparent">
+              Diseños Exclusivos
             </span>
           </div>
-          <h1 className="text-[3.85rem] uppercase leading-relaxed text-white">
-            Joyería de plata autentica
+          <h1 className="text-5xl font-semibold uppercase leading-snug tracking-widest text-white">
+            Joyas de plata
           </h1>
-          <p className="text-lg text-white">
+          <p className="balanc text-lg text-white">
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Saepe
-            repudiandae nobis quibusdam error eius minima iste facilis adipisci,
-            doloremque numquam officia culpa qui deleniti beatae. Repellat
-            quaerat fuga voluptatum ad.
+            repudiandae nobis quibusdam error eius minima iste facilis adipisci.
           </p>
-          <div className="font-ebGaramond mt-5 flex gap-5">
-            <button type="button" className="border px-8 py-4 text-white">
+          <div className="mt-4 flex gap-5 font-ebGaramond">
+            <button
+              type="button"
+              className="border bg-white px-10 py-2 font-ebGaramond text-xl italic tracking-wide text-black duration-150 hover:border-secondary hover:bg-secondary hover:text-white hover:transition-all hover:duration-75"
+            >
               Ver Catálogo
             </button>
-            <button type="button" className="border px-8 py-4 text-white">
-              Ver Catálogo
+            <button
+              type="button"
+              className="border px-10 py-2 font-ebGaramond text-xl italic text-white duration-150 hover:border-yellow-600 hover:bg-yellow-600 hover:text-black hover:transition-all hover:duration-75"
+            >
+              Contáctanos
             </button>
           </div>
         </div>
+      </div>
+
+      <div className="flex justify-evenly gap-8 px-32 py-8">
+        <a href="#" className="group relative flex items-center justify-center">
+          <img src={neckless} alt="" srcset="" className="relative z-10" />
+          <span className="absolute font-ebGaramond text-3xl font-bold italic text-white transition duration-300 group-hover:z-20 ">
+            Cadenas
+          </span>
+          <div className="absolute inset-0 z-10 bg-black bg-opacity-0 transition duration-300 ease-in-out group-hover:bg-opacity-50"></div>
+        </a>
+        <a href="#" className="group relative flex items-center justify-center">
+          <img src={earrings} alt="" srcset="" className="relative z-10" />
+          <span className="absolute font-ebGaramond text-3xl font-bold italic text-white transition duration-300 group-hover:z-20 ">
+            Colgantes
+          </span>
+          <div className="absolute inset-0 z-10 bg-black bg-opacity-0 transition duration-300 ease-in-out group-hover:bg-opacity-50"></div>
+        </a>
+        <a href="#" className="group relative flex items-center justify-center">
+          <img src={bracelet} alt="" srcset="" className="relative z-10" />
+          <span className="absolute font-ebGaramond text-3xl font-bold italic text-white transition duration-300 group-hover:z-20 ">
+            Brazaletes
+          </span>
+          <div className="absolute inset-0 z-10 bg-black bg-opacity-0 transition duration-300 ease-in-out group-hover:bg-opacity-50"></div>
+        </a>
+        <a href="#" className="group relative flex items-center justify-center">
+          <img src={ring} alt="" srcset="" className="relative z-10" />
+          <span className="absolute font-ebGaramond text-3xl font-bold italic text-white transition duration-300 group-hover:z-20 ">
+            Anillos
+          </span>
+          <div className="absolute inset-0 z-10 bg-black bg-opacity-0 transition duration-300 ease-in-out group-hover:bg-opacity-50"></div>
+        </a>
+        <a href="#" className="group relative flex items-center justify-center">
+          <img src={anklet} alt="" srcset="" className="relative z-10" />
+          <span className="absolute font-ebGaramond text-3xl font-bold italic text-white transition duration-300 group-hover:z-20 ">
+            Tobilleras
+          </span>
+          <div className="absolute inset-0 z-10 bg-black bg-opacity-0 transition duration-300 ease-in-out group-hover:bg-opacity-50"></div>
+        </a>
       </div>
     </div>
   );
--- src/assets/index.js
+++ src/assets/index.js
@@ -1,5 +1,19 @@
 import isotipo from "./isotipo.svg";
 import logotipo from "./logo.png";
 import BannerPrincipal from "./BannerPrincipal.webp";
+import neckless from "./Neckless.webp";
+import anklet from "./Anklet.png";
+import bracelet from "./Bracelet.png";
+import earrings from "./Earrings.png";
+import ring from "./Ring.png";
 
-export { isotipo, logotipo, BannerPrincipal };
+export {
+  isotipo,
+  logotipo,
+  BannerPrincipal,
+  neckless,
+  anklet,
+  bracelet,
+  earrings,
+  ring,
+};
