7603859697ec658114b878cf8010d1f7af51bc66
EugenioEscobar <eugenio.escobar18@gmail.com>
2024-01-08 08:40:14 -0300

    Layout changes

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -3,30 +3,26 @@ import { Link } from "react-router-dom";
 import { productos } from "../constants/products";
 import { LinkToProductos } from "./LinkToProducts";
 
-export function ProductList({ counter = 16, categoria }) {
+export function ProductList({ counter = 16, categoria, classname = "" }) {
   // if (categoria)
-  console.log("Productos");
-  console.log(productos.filter((prod) => prod.tipo === categoria));
-  const productListed = productos.slice(0, counter);
+  let productListed = productos.filter((prod) => prod.tipo === categoria);
+  productListed = productListed.slice(0, counter);
   return (
-    <>
+    <div>
       {categoria ? (
-        <span className="text flex justify-start px-36 py-10 text-4xl font-light uppercase">
+        <span className="text flex justify-start px-32 pt-16 text-4xl font-light uppercase">
           {categoria}
         </span>
       ) : null}
 
-      <main className="flex justify-center px-16  pt-4 md:px-32">
-        <div className="flex flex-1 flex-wrap justify-between px-12">
+      <main className={`flex justify-center px-16 pt-4 md:px-32 ${classname}`}>
+        <div className="grid grid-cols-6 gap-2">
           {productListed.map((item) => {
             return (
-              <article
-                key={item.id}
-                className="sm:w-[46%] md:w-[30%] lg:w-[22%]"
-              >
+              <article key={item.id} className="group bg-gray-50">
                 <Link
-                  to="/joya"
-                  className="group relative w-full flex-grow rounded-sm pb-16 hover:bg-slate-100"
+                  to={`/joya/${item.id}`}
+                  className="relative w-full flex-grow rounded-sm pb-16"
                 >
                   <img
                     src={neckless}
@@ -50,6 +46,6 @@ export function ProductList({ counter = 16, categoria }) {
       {categoria ? (
         <LinkToProductos categoria={categoria}></LinkToProductos>
       ) : null}
-    </>
+    </div>
   );
 }
